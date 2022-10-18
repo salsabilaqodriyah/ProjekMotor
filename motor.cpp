fe405dc2d2f83ec0433f086fbf9c568c1bfa38c3
salsabilaqodriyah <cacaqodriyah@gmail.com>
2022-10-18 17:42:15 +0700

    Salsabila Qodriyah | E1E121091 Penambahan Ketiga

--- motor.cpp
+++ motor.cpp
@@ -348,3 +348,192 @@ void drawFrame()
     glutSolidSphere(0.2,160.0,180.0);
     glPopMatrix();
 }
+
+//Salsabila Qodriyah_E1E121091
+void gear( GLfloat inner_radius, GLfloat outer_radius, GLfloat width,GLint teeth, GLfloat tooth_depth )
+{
+    GLint i;
+    GLfloat r0, r1, r2;
+    GLfloat angle, da;
+    GLfloat u, v, len;
+    const double pi = 3.14159264;
+    r0 = inner_radius;
+    r1 = outer_radius - tooth_depth/2.0;
+    r2 = outer_radius + tooth_depth/2.0;
+    da = 2.0*pi / teeth / 4.0;
+    glShadeModel( GL_FLAT );
+    glNormal3f( 0.0, 0.0, 1.0 );
+
+    glBegin( GL_QUAD_STRIP );
+    for (i=0;i<=teeth;i++)
+    {
+        angle = i * 2.0*pi / teeth;
+        glVertex3f( r0*cos(angle), r0*sin(angle), width*0.5 );
+        glVertex3f( r1*cos(angle), r1*sin(angle), width*0.5 );
+        glVertex3f( r0*cos(angle), r0*sin(angle), width*0.5 );
+        glVertex3f( r1*cos(angle+3*da), r1*sin(angle+3*da), width*0.5 );
+    }
+    glEnd();
+
+    glBegin( GL_QUADS );
+    da = 2.0*pi / teeth / 4.0;
+    for (i=0;i<teeth;i++)
+    {
+        angle = i * 2.0*pi / teeth;
+        glVertex3f( r1*cos(angle), r1*sin(angle), width*0.5 );
+        glVertex3f( r2*cos(angle+da), r2*sin(angle+da), width*0.5 );
+        glVertex3f( r2*cos(angle+2*da), r2*sin(angle+2*da), width*0.5 );
+        glVertex3f( r1*cos(angle+3*da), r1*sin(angle+3*da), width*0.5 );
+    }
+    glEnd();
+    glNormal3f( 0.0, 0.0, -1.0 );
+
+    glBegin( GL_QUAD_STRIP );
+    for (i=0;i<=teeth;i++)
+    {
+        angle = i * 2.0*pi / teeth;
+        glVertex3f( r1*cos(angle), r1*sin(angle), -width*0.5 );
+        glVertex3f( r0*cos(angle), r0*sin(angle), -width*0.5 );
+        glVertex3f( r1*cos(angle+3*da), r1*sin(angle+3*da), -width*0.5 );
+        glVertex3f( r0*cos(angle), r0*sin(angle), -width*0.5 );
+    }
+    glEnd();
+
+    glBegin( GL_QUADS );
+    da = 2.0*pi / teeth / 4.0;
+    for (i=0;i<teeth;i++)
+    {
+        angle = i * 2.0*pi / teeth;
+        glVertex3f( r1*cos(angle+3*da), r1*sin(angle+3*da), -width*0.5 );
+        glVertex3f( r2*cos(angle+2*da), r2*sin(angle+2*da), -width*0.5 );
+        glVertex3f( r2*cos(angle+da), r2*sin(angle+da), -width*0.5 );
+        glVertex3f( r1*cos(angle), r1*sin(angle), -width*0.5 );
+    }
+    glEnd();
+
+    glBegin( GL_QUAD_STRIP );
+    for (i=0;i<teeth;i++)
+    {
+        angle = i * 2.0*pi / teeth;
+        glVertex3f( r1*cos(angle), r1*sin(angle), width*0.5 );
+        glVertex3f( r1*cos(angle), r1*sin(angle), -width*0.5 );
+        u = r2*cos(angle+da) - r1*cos(angle);
+        v = r2*sin(angle+da) - r1*sin(angle);
+        len = sqrt( u*u + v*v );
+        u /= len;
+        v /= len;
+        glNormal3f( v, -u, 0.0 );
+        glVertex3f( r2*cos(angle+da), r2*sin(angle+da), width*0.5 );
+        glVertex3f( r2*cos(angle+da), r2*sin(angle+da), -width*0.5 );
+        glNormal3f( cos(angle), sin(angle), 0.0 );
+        glVertex3f( r2*cos(angle+2*da), r2*sin(angle+2*da), width*0.5 );
+        glVertex3f( r2*cos(angle+2*da), r2*sin(angle+2*da), -width*0.5 );
+        u = r1*cos(angle+3*da) - r2*cos(angle+2*da);
+        v = r1*sin(angle+3*da) - r2*sin(angle+2*da);
+        glNormal3f( v, -u, 0.0 );
+        glVertex3f( r1*cos(angle+3*da), r1*sin(angle+3*da), width*0.5 );
+        glVertex3f( r1*cos(angle+3*da), r1*sin(angle+3*da), -width*0.5 );
+        glNormal3f( cos(angle), sin(angle), 0.0 );
+    }
+        glVertex3f( r1*cos(0.0), r1*sin(0.0), width*0.5 );
+        glVertex3f( r1*cos(0.0), r1*sin(0.0), -width*0.5 );
+        glEnd();
+        glShadeModel( GL_SMOOTH );
+
+        glBegin( GL_QUAD_STRIP );
+        for (i=0;i<=teeth;i++)
+        {
+            angle = i * 2.0*pi / teeth;
+            glNormal3f( -cos(angle), -sin(angle), 0.0 );
+            glVertex3f( r0*cos(angle), r0*sin(angle), -width*0.5 );
+            glVertex3f( r0*cos(angle), r0*sin(angle), width*0.5 );
+        }
+        glEnd();
+}
+
+void drawChain()
+{
+    GLfloat depth;
+    static int mode=0;
+    glColor3f(0.0f,1.0f,0.0f);
+    glEnable(GL_LINE_STIPPLE);
+    mode=(mode+1)%2;
+
+    if(mode==0 && speed>0)
+    glLineStipple(1,0x1c47);
+    else if(mode==1 && speed>0)
+    glLineStipple(1,0x00FF);
+    glBegin(GL_LINES);
+    for(depth=0.06f;depth<=0.12f;depth+=0.01f)
+    {
+        glVertex3f(-1.6f,0.15f,ROD_RADIUS);
+        glVertex3f(0.0f,0.3f,depth);
+
+        glVertex3f(-1.6f,-0.15f,ROD_RADIUS);
+        glVertex3f(0.0f,-0.3f,depth);
+    }
+    glEnd();
+    glDisable(GL_LINE_STIPPLE);
+}
+
+
+void drawSeat()
+{
+
+    glBegin(GL_POLYGON);
+    glVertex3f(-0.1f, 1.0f, -0.5f);
+    glVertex3f( 1.0f, 1.0f, -0.3f);
+    glVertex3f( 1.0f, 1.0f, 0.3f);
+    glVertex3f(-0.1f, 1.0f, 0.5f);
+    glVertex3f(-0.5f, 1.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f, -1.0f);
+    glVertex3f(-0.5f, 1.0f, -1.0f);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glVertex3f(-0.1f, -1.0f, -0.5f);
+    glVertex3f( 1.0f, -1.0f, -0.3f);
+    glVertex3f( 1.0f, -1.0f, 0.3f);
+    glVertex3f(-0.1f, -1.0f, 0.5f);
+    glVertex3f(-0.5f, -1.0f, 1.0f);
+    glVertex3f(-1.0f, -1.0f, 1.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);
+    glVertex3f(-0.5f, -1.0f, -1.0f);
+    glEnd();
+
+    glBegin(GL_QUADS);
+    glVertex3f(1.0f,1.0f,-0.3f);
+    glVertex3f(1.0f,1.0f,0.3f);
+    glVertex3f(1.0f,-1.0f,0.3f);
+    glVertex3f(1.0f,-1.0f,-0.3f);
+    glVertex3f(1.0f,1.0f,0.3f);
+    glVertex3f(-0.1f,1.0f,0.5f);
+    glVertex3f(-0.1f,-1.0f,0.5f);
+    glVertex3f(1.0f,-1.0f,0.3f);
+    glVertex3f(1.0f,1.0f,-0.3f);
+    glVertex3f(-0.1f,1.0f,-0.5f);
+    glVertex3f(-0.1f,-1.0f,-0.5f);
+    glVertex3f(1.0f,-1.0f,-0.3f);
+    glVertex3f(-0.1f,1.0f,0.5f);
+    glVertex3f(-0.5f,1.0f,1.0f);
+    glVertex3f(-0.5f,-1.0f,1.0f);
+    glVertex3f(-0.1f,-1.0f,0.5f);
+    glVertex3f(-0.1f,1.0f,-0.5f);
+    glVertex3f(-0.5f,1.0f,-1.0f);
+    glVertex3f(-0.5f,-1.0f,-1.0f);
+    glVertex3f(-0.1f,-1.0f,-0.5f);
+    glVertex3f(-0.5f,1.0f,1.0f);
+    glVertex3f(-1.0f,1.0f,1.0f);
+    glVertex3f(-1.0f,-1.0f,1.0f);
+    glVertex3f(-0.5f,-1.0f,1.0f);
+    glVertex3f(-0.5f,1.0f,-1.0f);
+    glVertex3f(-1.0f,1.0f,-1.0f);
+    glVertex3f(-1.0f,-1.0f,-1.0f);
+    glVertex3f(-0.5f,-1.0f,-1.0f);
+    glVertex3f(-1.0f,1.0f,1.0f);
+    glVertex3f(-1.0f,1.0f,-1.0f);
+    glVertex3f(-1.0f,-1.0f,-1.0f);
+    glVertex3f(-1.0f,-1.0f,1.0f);
+    glEnd();
+}
